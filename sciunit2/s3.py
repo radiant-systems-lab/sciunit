7d6dc38f8cae0b585b6ddd1088cf1ece17e98da2
Talha Azaz <talha129c@gmail.com>
2025-09-17 17:57:49 -0500

    added CF url

--- sciunit2/s3.py
+++ sciunit2/s3.py
@@ -8,22 +8,22 @@ import urllib
 
 
 
-CF_DOMAIN  = "dxxxxxxxxxxxxx.cloudfront.net"
+CF_DOMAIN  = "https://d3okuktvxs1y4w.cloudfront.net"
 
 def live(fn, bucket="sciunit2-talha"):
     """
-    Uploads a file to S3 and returns a pre-signed download URL.
+    Uploads a file to S3 and returns a CF download URL.
     """
     s3 = boto3.client('s3')
-    key = datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "/" + fn
+    key = datetime.now().strftime("%Y-%m-%d-%H:%M:%S") + "/" + fn
     s3.upload_file(fn, bucket, key)
-    cf_url = f"https://{CF_DOMAIN}/{key}"
+    cf_url = f"{CF_DOMAIN}/{key}"
     return cf_url
     
 @retry(urllib.error.HTTPError, tries=3, delay=0.3, backoff=2)
 def fetch(url, base):
     """
-    Downloads a file from a pre-signed S3 URL and returns a file-like object.
+    Downloads a file from a CF URL and returns a file-like object.
     """
     import requests
     with requests.get(url, stream=True) as resp:
